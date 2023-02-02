1ca5ce987b6eb72a34ca81cf60bf34673b842e3d
zzztttkkk <ztkisalreadytaken@gmail.com>
2023-02-02 18:33:45 +0800

    [🐶chore] (/): -

--- src/http/fs.rs
+++ src/http/fs.rs
@@ -1,6 +1,8 @@
+use std::future::Future;
 use std::io::{ErrorKind, Write};
 
 use async_trait::async_trait;
+use tokio::fs::ReadDir;
 
 use crate::http::context::Context;
 use crate::http::handler::Handler;
@@ -8,6 +10,8 @@ use crate::http::handler::Handler;
 pub struct FsHandler {
     root: String,
     prefix: String,
+    disable_index: bool,
+    index_render: Option<Box<dyn (Fn(&Vec<tokio::fs::DirEntry>) -> String) + Send + Sync>>,
 }
 
 impl FsHandler {
@@ -34,11 +38,79 @@ impl FsHandler {
             prefix.push('/');
         }
 
-        Box::new(Self { root, prefix })
+        Box::new(Self {
+            root,
+            prefix,
+            disable_index: false,
+            index_render: None,
+        })
     }
 
     async fn index(&self, fp: &str, metadata: &std::fs::Metadata, ctx: &mut Context) {
         println!("Index: {}", fp);
+
+        let index_path = format!("{}/index.html", fp);
+        if let Ok(index_metadata) = (tokio::fs::metadata(&index_path).await) {
+            self.file(&index_path, &index_metadata, ctx).await;
+            return;
+        }
+
+        match tokio::fs::read_dir(fp).await {
+            Err(_) => {
+                ctx.response().statuscode(404);
+                return;
+            }
+            Ok(ref mut iter) => {
+                let mut ents = Vec::with_capacity(10);
+                loop {
+                    let item = iter.next_entry().await;
+                    match item {
+                        Err(_) => {
+                            break;
+                        }
+                        Ok(opt) => match opt {
+                            Some(ent) => {
+                                ents.push(ent);
+                            }
+                            None => {
+                                break;
+                            }
+                        },
+                    }
+                }
+
+                match &self.index_render {
+                    Some(rfn) => {
+                        rfn(&ents);
+                    }
+                    None => {
+                        let mut resp = ctx.response();
+                        resp.headers().set_content_type("text/html");
+
+                        _ = resp.write("<html><document><body><ol>".as_bytes());
+                        for ele in &ents {
+                            if let Some(filename) = ele.file_name().to_str() {
+                                if let Ok(metadate) = (ele.metadata().await) {
+                                    if metadata.is_file() {
+                                        _ = resp.write(
+                                            format!("<li>./{} {}</li>", filename, metadata.len())
+                                                .as_bytes(),
+                                        );
+                                        continue;
+                                    }
+                                    if metadata.is_dir() {
+                                        _ = resp
+                                            .write(format!("<li>./{}/</li>", filename).as_bytes());
+                                        continue;
+                                    }
+                                }
+                            }
+                        }
+                        _ = resp.write("</ol></body></document></html>".as_bytes());
+                    }
+                }
+            }
+        }
     }
 
     async fn file(&self, fp: &str, metadata: &std::fs::Metadata, ctx: &mut Context) {
@@ -71,7 +143,11 @@ impl Handler for FsHandler {
             }
             Ok(ref metadata) => {
                 if metadata.is_dir() {
-                    self.index(fs_path.as_str(), metadata, ctx).await;
+                    if self.disable_index {
+                        ctx.response().statuscode(404);
+                    } else {
+                        self.index(fs_path.as_str(), metadata, ctx).await;
+                    }
                     return;
                 }
                 if metadata.is_file() {
